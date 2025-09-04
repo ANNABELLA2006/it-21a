01e6872583192a6ae53a767592833a867cf7b5e2
ANNABELLA <annabellasuposo2006@gmail.com>
2025-09-04 11:04:48 -0800

    Added a factorial.js

---
+++ factorial.js
@@ -0,0 +1,28 @@
+const readline = require ("readline");
+
+const rl = readline.createInterface ( {
+    input:Process.stdin,
+    output:Process.stdout
+});
+
+
+
+console.log("factorial Application");
+
+console.log("1. Say Hello");
+console.log("2. Compute Factorial");
+console.log("3. Exit");
+
+rl.question("Enter your choice (1-3)", choice =>{
+    if (choice == "1") {
+        console.log("Hello There");
+    }else if (choice === "2") {
+        console.log("Factorial");
+    }else if (choice === "3") {
+        console.log("Exit");
+    }else {
+        console.log("Invalid Choice. Please try again.")
+    } 
+
+    
+})
